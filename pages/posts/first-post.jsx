625768091fd9c1b75cececb7492f3916cef0c23b
Luis Cadillo <lusicaf3r@Gmail.com>
2022-04-20 20:46:19 -0500

    Add global style at both page level and app level

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,18 +1,26 @@
 import Link from 'next/link';
+import Head from 'next/head';
+
+import Layout from '../../components/Layout';
 
 const FirstPost = () => {
   return (
-    <div>
-      <h1>First Post</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        doloremque, quidem quisquam, quisquam quisquam quisquam quisquam
-        dignissimos.
-      </p>
-      <Link href="/">
-        <a>Go to Home</a>
-      </Link>
-    </div>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <div>
+        <h1>First Post</h1>
+        <p className='card'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          doloremque, quidem quisquam, quisquam quisquam quisquam quisquam
+          dignissimos.
+        </p>
+        <Link href="/">
+          <a>Go to Home</a>
+        </Link>
+      </div>
+    </Layout>
   );
 };
 
